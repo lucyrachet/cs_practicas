9a1e454186b76977088483a51cc01bc3112e6fa4
TheMoratilla <luis.porata@outlook.com>
2021-11-13 20:23:03 +0100

    a

--- PanelInicio.java
+++ PanelInicio.java
@@ -27,9 +27,9 @@ public class PanelInicio extends javax.swing.JPanel {
         encriptarResult.setText("");
         desencriptarResult.setText("");
         openEncriptar = new JFileChooser();
-        openEncriptar.setCurrentDirectory(new File("\\desencriptado"));
+        openEncriptar.setCurrentDirectory(new File("/desencriptado"));
         openDesencriptar = new JFileChooser();
-        openDesencriptar.setCurrentDirectory(new File("\\encriptado"));
+        openDesencriptar.setCurrentDirectory(new File("/encriptado"));
         openDesencriptar.setFileFilter(new FileNameExtensionFilter("Encryted files","enc"));
     }
 
